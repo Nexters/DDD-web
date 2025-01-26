09f1ba0b14dbf5e0da635889c7bd621474273c5e
Jay Lee <roof2586@naver.com>
2025-01-26 15:08:17 +0900

    feat: 타로 카드 선택 API 함수 및 mock API 구현

---
+++ src/app/api/v1/tarot/select/route.ts
@@ -0,0 +1,18 @@
+import { SelectTarotCardRequest, SelectTarotCardResponse } from '@/tarot/services/apis/selectTarotCard';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  const body: SelectTarotCardRequest = await request.json();
+  console.log(body);
+
+  const mockData: SelectTarotCardResponse = {
+    messageId: 1,
+    type: 'USER_NORMAL',
+    sender: 'SYSTEM',
+    answer: ['타로결과를 다시 보고 싶으면 카드를 눌러보라냥🐾', '또 궁금한 거 있어냥?'],
+    tarotName: 'M_00',
+    tarotResultId: 1,
+  };
+
+  return NextResponse.json(mockData);
+}
