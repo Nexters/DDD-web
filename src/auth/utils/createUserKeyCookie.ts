4fe9c2842c5ca219cfeb3c52edcc1160345a2948
Jay Lee <roof2586@naver.com>
2025-01-26 22:27:13 +0900

    feat: userKey 쿠키의 sameSite 속성을 "none"으로 변경

--- src/auth/utils/createUserKeyCookie.ts
+++ src/auth/utils/createUserKeyCookie.ts
@@ -12,6 +12,10 @@ export const createUserKeyCookie = () => {
     httpOnly: true,
     secure: true,
     maxAge: 60 * 60 * 24 * 365,
+    /**
+     * TODO: 백엔드 서버와 도메인을 통합한 후 설정값 변경
+     */
+    sameSite: 'none',
   });
 
   return userKey;
