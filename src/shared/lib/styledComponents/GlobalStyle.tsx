1e20149666f5fec59da5847135c52e84ad7166a2
Jay Lee <roof2586@naver.com>
2025-01-26 14:07:23 +0900

    feat: 전체 화면 배경색 제거

--- src/shared/lib/styledComponents/GlobalStyle.tsx
+++ src/shared/lib/styledComponents/GlobalStyle.tsx
@@ -19,12 +19,6 @@ select,
 textarea {
   font-family: inherit;
 }
-
-@media screen and (min-width: 450px) {
-  body {
-    background-color: rgb(245, 245, 245);
-  }
-}
 `;
 
 export default GlobalStyle;
