49607ff108869f126be2457edc76e7214f4da4fd
Jay Lee <roof2586@naver.com>
2025-01-26 20:11:18 +0900

    feat: 채팅 도메인 쿼리 및 뮤테이션 훅 추가
    채팅 기능을 위한 React Query 훅 생성
    - useChatMessages: 채팅 메시지 조회 쿼리 훅
    - useCreateChatRoom: 채팅방 생성 뮤테이션 훅
    - useSendChatMessage: 채팅 메시지 전송 뮤테이션 훅

---
+++ src/chat/hooks/useChatMessages.ts
@@ -0,0 +1,9 @@
+import { useQuery } from '@tanstack/react-query';
+import { getChatMessagesByRoomId } from '../apis/getChatMessagesByRoomId';
+
+export const useChatMessages = (roomId: number) => {
+  return useQuery({
+    queryKey: ['chatMessages', roomId],
+    queryFn: () => getChatMessagesByRoomId(roomId),
+  });
+};
