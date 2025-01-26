49659497ef775a6fcb29dc00c8cbbc213daf9566
Jay Lee <roof2586@naver.com>
2025-01-26 14:32:07 +0900

    reface: 불필요한 요청 필드 제거

--- src/shared/apis/logShareEvent.ts
+++ src/shared/apis/logShareEvent.ts
@@ -2,12 +2,10 @@ import apiClient from '../lib/axios/apiClient';
 
 export type LogShareEventRequest = {
   resultId: number;
-  target: string;
 };
 
-export const logShareEvent = ({ resultId, target }: LogShareEventRequest) => {
+export const logShareEvent = ({ resultId }: LogShareEventRequest) => {
   return apiClient.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/common/share`, {
     resultId,
-    target,
   });
 };
