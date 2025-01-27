133288ff77453261592ab34ce1a1d096f647f764
Jay Lee <roof2586@naver.com>
2025-01-27 23:28:23 +0900

    feat: 추천 목록 데이터 프리페치 SSR 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,18 @@
-'use client';
+import ChatOverview from '@/chat/components/ChatOverview';
+import { getTarotQuestionRecommends } from '@/tarot/apis/getTarotQuestionRecommends';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
-import QuickQuestionPickerBox from '@/chat/components/QuickQuestionPickerBox';
-import MainContent from '@/shared/components/MainContent';
-import { css } from 'styled-components';
+export default async function HomePage() {
+  const queryClient = new QueryClient();
 
-export default function HomePage() {
+  await queryClient.prefetchQuery({
+    queryKey: ['tarotQuestionRecommends'],
+    queryFn: getTarotQuestionRecommends,
+  });
+
   return (
-    <>
-      <MainContent>
-        <h1
-          css={css`
-            margin-top: 170px;
-            text-align: center;
-            ${(props) => props.theme.fonts.headline2}
-          `}
-        >
-          AI 타로 술사, 타로냥에게
-          <br /> 무엇이든 물어봐라냥
-        </h1>
-        <div
-          css={css`
-            margin-top: 32px;
-          `}
-        >
-          <QuickQuestionPickerBox />
-        </div>
-      </MainContent>
-    </>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ChatOverview />
+    </HydrationBoundary>
   );
 }
