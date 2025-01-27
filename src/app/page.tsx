7e98822db12257a9f634191108f67d8329319a4e
Jay Lee <roof2586@naver.com>
2025-01-27 22:48:27 +0900

    feat: 인덱스 페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
-import HeaderContent from '@/shared/components/HeaderContent';
+'use client';
+
+import QuickQuestionPickerBox from '@/chat/components/QuickQuestionPickerBox';
 import MainContent from '@/shared/components/MainContent';
+import { css } from 'styled-components';
 
 export default function HomePage() {
   return (
     <>
-      <HeaderContent>{null}</HeaderContent>
-      <MainContent>{null}</MainContent>
+      <MainContent>
+        <h1
+          css={css`
+            margin-top: 170px;
+            text-align: center;
+            ${(props) => props.theme.fonts.headline2}
+          `}
+        >
+          AI 타로 술사, 타로냥에게
+          <br /> 무엇이든 물어봐라냥
+        </h1>
+        <div
+          css={css`
+            margin-top: 32px;
+          `}
+        >
+          <QuickQuestionPickerBox />
+        </div>
+      </MainContent>
     </>
   );
 }
