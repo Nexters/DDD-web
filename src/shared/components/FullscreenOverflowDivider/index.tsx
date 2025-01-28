a9f663dfea0ac77996cce759e13dbe8cdf710280
Jay Lee <roof2586@naver.com>
2025-01-28 15:11:03 +0900

    feat: 전체 화면 오버플로우 디바이더 컴포넌트 추가

---
+++ src/shared/components/FullscreenOverflowDivider/index.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { css } from "styled-components";
+
+export default function FullscreenOverflowDivider() {
+  return (
+    <hr
+      css={css`
+        margin: 0;
+        block-size: 1px;
+        border: none;
+        width: 100%;
+        background-color: ${(props) => props.theme.colors.grey10};
+        box-shadow: 0 0 0 100vmax ${(props) => props.theme.colors.grey10};
+        clip-path: inset(0px -100vmax);
+      `}
+    />
+  );
+}
