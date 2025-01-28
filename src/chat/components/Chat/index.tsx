ca5714f659a3242747ebb6da8e6bf04ad3e2cbfa
Jay Lee <roof2586@naver.com>
2025-01-29 02:06:26 +0900

    chore: 투두 주석 추가

--- src/chat/components/Chat/index.tsx
+++ src/chat/components/Chat/index.tsx
@@ -5,6 +5,7 @@ import ChatHeader from "../ChatHeader";
 import ChatRoom from "../ChatRoom";
 
 export default function Chat() {
+  // TODO: 채팅 메세지 목록 프리페치 SSR 필요
   return (
     <>
       <ChatHeader />
