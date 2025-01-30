d1e4fe30638f430ab27b71a2ffe174f7aaec7e3e
ljh0608 <m98m@naver.com>
2025-01-30 18:49:33 +0900

    feat: 카드 Left Move animation

--- src/chat/components/Card.tsx
+++ src/chat/components/Card.tsx
@@ -5,13 +5,22 @@ import styled from "styled-components";
 import CardBack from "@/shared/assets/images/cardBack.webp";
 import { cubicBezier } from "motion";
 import { div } from "motion/react-client";
-import { useState } from "react";
+import { SetStateAction, useState, Dispatch } from "react";
+import { useRef, useEffect } from "react";
 interface PropTypes {
   idx: number;
+  animationTrigger: any;
 }
 
-const Card = ({ idx }: PropTypes) => {
+const moveCardDeck = {
+  duration: 0.5,
+  delay: 1.5,
+};
+
+const Card = ({ idx, animationTrigger }: PropTypes) => {
   const [isCardShadow, setIsCardShadow] = useState(false);
+  const [moveDistance, setMoveDistance] = useState(0);
+  const cardRef = useRef<HTMLDivElement>(null);
 
   const CardSpread = {
     x: [0, idx * 50],
@@ -22,26 +31,52 @@ const Card = ({ idx }: PropTypes) => {
     },
   };
 
+  const onAnimationEnd = () => {
+    setIsCardShadow(true);
+  };
+
+  useEffect(() => {
+    if (cardRef.current) {
+      const grandParentElement = cardRef.current.parentElement?.parentElement; // 부모의 부모 요소 접근
+      console.log(grandParentElement);
+      const grandParentWidth = grandParentElement?.offsetWidth || 0;
+      console.log(grandParentWidth);
+      const cardWidth = cardRef.current.offsetWidth;
+
+      setMoveDistance(-(grandParentWidth / 2 + cardWidth / 2)); // 부모의 부모 기준 왼쪽 끝에 붙이기
+    }
+  }, []);
   return (
     <CardAnimationWrapper
-      zIndex={idx}
+      ref={cardRef}
       animate={CardSpread}
-      onAnimationComplete={() => setIsCardShadow(true)}
+      onAnimationComplete={onAnimationEnd}
     >
-      <CardWrapper
-        src={CardBack}
-        alt="카드 뒷면이미지"
-        isCardShadow={isCardShadow}
-      />
+      <CardMoveLeft
+        animate={{ translateX: moveDistance }}
+        transition={moveCardDeck}
+      >
+        <CardWrapper
+          src={CardBack}
+          alt="카드 뒷면이미지"
+          isCardShadow={isCardShadow}
+        />
+      </CardMoveLeft>
     </CardAnimationWrapper>
   );
 };
 
 export default Card;
+const CardMoveLeft = styled(div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
 
-const CardAnimationWrapper = styled(div)<{ zIndex: number }>`
-  z-index: ${({ zIndex }) => zIndex + 1};
+  height: 100%;
 `;
+
+const CardAnimationWrapper = styled(div)``;
 
 const CardWrapper = styled(Image)<{ isCardShadow: boolean }>`
   border-radius: 8px;
@@ -52,4 +87,6 @@ const CardWrapper = styled(Image)<{ isCardShadow: boolean }>`
   width: 100px;
   height: 160px;
   position: absolute;
+
+  cursor: pointer;
 `;
--- src/chat/components/ChatCardSelect.tsx
+++ src/chat/components/ChatCardSelect.tsx
@@ -1,32 +1,45 @@
 import styled from "styled-components";
 import * as motion from "motion/react-client";
 import { easeInOut } from "motion";
-
 import Card from "./Card";
 import { useState } from "react";
+
+import { useMotionValue } from "motion/react";
 interface PropTypes {
   onClick: () => void;
 }
 
 type CardState = "Deck" | "Spread" | "Pick";
 
-const RiseUpCardDeck = {
+const riseUpCardDeck = {
   duration: 0.6,
   ease: easeInOut,
 };
 
+// const MoveCardDeck = {
+
+//   left:[100%, 0],
+//   delay: 1.5,
+// };
+
 const ChatCardSelect = ({ onClick }: PropTypes) => {
   const [cardState, setCardState] = useState<CardState>("Deck");
+  const [cardDeckAnimation, setCardDeckAnimation] = useState<
+    "Rise" | "LeftMove"
+  >("Rise");
+
+  const x = useMotionValue("Rise");
   return (
     <CardDeckWrapper
       initial={{ opacity: 0, y: 200 }}
       //추후 0으로 변경 하고 parents에서 위치 조절
-      animate={{ opacity: 1, y: -20 }}
-      transition={RiseUpCardDeck}
+
+      animate={{ opacity: 1, y: 0 }}
+      transition={riseUpCardDeck}
       onAnimationComplete={() => setCardState("Spread")}
     >
       {Array.from({ length: 12 }).map((_, idx) => (
-        <Card key={idx} idx={idx} />
+        <Card key={idx} idx={idx} animationTrigger={x} />
       ))}
     </CardDeckWrapper>
   );
@@ -41,7 +54,6 @@ const CardDeckWrapper = styled(motion.div)`
 
   width: 100%;
   height: 400px;
-
   position: relative;
   overflow: hidden;
 `;
