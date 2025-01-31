95881203250cb5af9f00f647dca8cc9b3c5b3f0e
Jay Lee <roof2586@naver.com>
2025-01-31 22:54:26 +0900

    feat: ChatAvatar 컴포넌트에 프로필 이미지 추가

--- src/chat/components/ChatAvatar.tsx
+++ src/chat/components/ChatAvatar.tsx
@@ -1,15 +1,17 @@
+import ChatBotProfileImage from "@/shared/assets/images/chatbot-profile.webp";
+import Image from "next/image";
 import { css } from "styled-components";
-
 export default function ChatAvatar() {
   // TODO: 이미지로 교체
   return (
-    <div
+    <Image
+      src={ChatBotProfileImage}
+      alt="타로냥 프로필 이미지"
+      width={36}
+      height={36}
       css={css`
-        width: 36px;
-        height: 36px;
         border-radius: 50%;
         background-color: ${({ theme }) => theme.colors.grey10};
-        border: 1px solid ${({ theme }) => theme.colors.grey20};
       `}
     />
   );
