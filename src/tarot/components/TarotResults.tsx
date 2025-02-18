1200968145e5168e723d0c5b0b44b730398908c2
Jay Lee <roof2586@naver.com>
2025-02-18 22:27:57 +0900

    feat: 타로 결과 모아보기

--- src/tarot/components/TarotResults.tsx
+++ src/tarot/components/TarotResults.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import CatImage from "@/shared/assets/images/catInResultsPage.png";
-import Button from "@/shared/components/Button";
+import ChevronLeftIcon from "@/shared/assets/icons/chevron-left.svg";
 import Image from "next/image";
-import { useParams, useRouter } from "next/navigation";
+import Link from "next/link";
 import { css } from "styled-components";
 import { useTarotReadingResultList } from "../hooks/useTarotReadingResultList";
+import findCardById from "../utils/findCardById";
+import TarotResultsEmptyView from "./TarotResultsEmptyView";
 export default function TarotResults() {
   const { data } = useTarotReadingResultList();
-  const router = useRouter();
-  const { chatId } = useParams();
 
   if (!data) return null;
 
@@ -21,45 +20,80 @@ export default function TarotResults() {
         margin-inline: auto;
       `}
     >
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          margin-top: 180px;
-          gap: 12px;
-        `}
-      >
-        <div
-          css={css`
-            display: flex;
-            flex-direction: column;
-            gap: 36px;
-          `}
-        >
-          <Image src={CatImage} alt="" width={206} height={169} />
-          <p
-            css={css`
-              ${({ theme }) => theme.fonts.headline1};
-              color: ${({ theme }) => theme.colors.grey70};
-            `}
-          >
-            아직 본 타로 결과가 없다냥
-          </p>
-        </div>
-        <Button
-          color="grey70"
-          css={css`
-            width: 140px;
-            height: 54px;
-          `}
-          onClick={() => {
-            router.push(`/chats/${chatId}`);
-          }}
-        >
-          지금 타로 보기
-        </Button>
-      </div>
+      {data.results.length === 0 ? (
+        <TarotResultsEmptyView />
+      ) : (
+        <ul>
+          {data.results.map((result) => (
+            <li
+              key={result.id}
+              css={css`
+                border-bottom: 1px solid ${({ theme }) => theme.colors.grey20};
+                &:last-child {
+                  border-bottom: none;
+                }
+              `}
+            >
+              <Link
+                href={`/chats/${result.chatRoomId}/tarot-reading/${result.tarotResultId}`}
+                css={css`
+                  display: flex;
+                  padding: 20px;
+                  gap: 16px;
+                `}
+              >
+                <Image
+                  src={findCardById(result.selectedTarot).imgSrc}
+                  alt=""
+                  width={52}
+                  height={77}
+                  css={css`
+                    background-color: ${({ theme }) => theme.colors.grey20};
+                  `}
+                />
+                <div
+                  css={css`
+                    display: flex;
+                    flex: 1;
+                    gap: 16px;
+                  `}
+                >
+                  <div
+                    css={css`
+                      display: flex;
+                      flex-direction: column;
+                      gap: 4px;
+                      flex: 1;
+                    `}
+                  >
+                    <p
+                      css={css`
+                        ${({ theme }) => theme.fonts.body1};
+                        color: ${({ theme }) => theme.colors.grey50};
+                      `}
+                    >
+                      {new Date(result.createdAt).toLocaleDateString()}
+                    </p>
+                    <p
+                      css={css`
+                        ${({ theme }) => theme.fonts.subHead3};
+                        color: ${({ theme }) => theme.colors.grey90};
+                        overflow: hidden;
+                        display: -webkit-box;
+                        -webkit-box-orient: vertical;
+                        -webkit-line-clamp: 2;
+                      `}
+                    >
+                      {result.questionSummary}
+                    </p>
+                  </div>
+                  <ChevronLeftIcon style={{ transform: "rotate(180deg)", alignSelf: "center" }} />
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
