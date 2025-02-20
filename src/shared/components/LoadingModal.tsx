3fee6c6f482a0b294eed8b60fd37c71fefdcc42a
Jay Lee <roof2586@naver.com>
2025-02-21 00:38:08 +0900

    feat: 홈, 로딩 UI에 로띠 추가

--- src/shared/components/LoadingModal.tsx
+++ src/shared/components/LoadingModal.tsx
@@ -1,14 +1,14 @@
+import loadingAnimation from "@/shared/assets/lotties/loading-cat.json";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
-import { css, keyframes, styled, useTheme } from "styled-components";
+import Lottie from "lottie-react";
+import { css } from "styled-components";
 import Modal from "./Modal";
-
 interface LoadingModalProps {
   isOpen: boolean;
   onOpenChange?: (open: boolean) => void;
 }
 
 export default function LoadingModal({ isOpen, onOpenChange }: LoadingModalProps) {
-  const theme = useTheme();
   return (
     <Modal.Root isOpen={isOpen} onOpenChange={onOpenChange}>
       <Modal.Portal>
@@ -30,32 +30,9 @@ export default function LoadingModal({ isOpen, onOpenChange }: LoadingModalProps
             <Modal.Description>잠시만 기다려주세요.</Modal.Description>
           </VisuallyHidden>
 
-          <Dot $delay={0} $color={theme.colors.primary01} />
-          <Dot $delay={0.3} $color={theme.colors.primary02} />
-          <Dot $delay={0.6} $color={theme.colors.primary03} />
+          <Lottie animationData={loadingAnimation} loop={true} style={{ height: 500 }} />
         </Modal.Content>
       </Modal.Portal>
     </Modal.Root>
   );
 }
-
-const fadeInOut = keyframes`
-  0% {
-    opacity: 0;
-  }
-  50% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0;
-  }
-`;
-
-const Dot = styled.span<{ $delay: number; $color: string }>`
-  width: 10px;
-  height: 10px;
-  background-color: ${({ $color }) => $color};
-  border-radius: 50%;
-  animation: ${fadeInOut} 1.5s infinite ease-in-out;
-  animation-delay: ${({ $delay }) => $delay}s;
-`;
