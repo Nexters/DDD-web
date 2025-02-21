04efdb50e3985fa3857142a47c8d61d6df8ca7ce
ljh0608 <m98m@naver.com>
2025-02-22 05:59:14 +0900

    fix 리뷰 여부 owner일때만 요청하도록 수정

--- src/tarot/hooks/useTarotReviewExist.ts
+++ src/tarot/hooks/useTarotReviewExist.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getReviewExist } from "../apis/getReviewExist";
 
-export const useTarotReviewExist = (requestId: number) => {
+export const useTarotReviewExist = (requestId: number, isOwner: boolean | undefined) => {
   return useQuery({
     queryKey: ["tarotReviewExist"],
     queryFn: () => getReviewExist(requestId),
+    enabled: !!isOwner,
   });
 };
