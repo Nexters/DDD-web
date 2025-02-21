c75299573b9555d5c26a9b4eabbb37914443e030
Jay Lee <roof2586@naver.com>
2025-02-21 22:20:06 +0900

    feat: mixpanel debug 설정 제거

--- src/shared/lib/mixpanel/index.tsx
+++ src/shared/lib/mixpanel/index.tsx
@@ -6,7 +6,6 @@ import { useEffect } from "react";
 export default function MixpanelSDKInitializer() {
   useEffect(() => {
     mixpanel.init("206953e53aebe08b0d26725c2b4ee1f0", {
-      debug: true,
       track_pageview: true,
       persistence: "localStorage",
       record_sessions_percent: 100,
